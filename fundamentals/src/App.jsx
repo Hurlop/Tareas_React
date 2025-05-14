50f8a81e28178fd95d657a44dcc768fd8b8eba2e
Hurlop <daniel99020602844@gmail.com>
2025-05-13 21:38:31 -0500

    learning the basics and creating a new doc from scratch

--- fundamentals/src/App.jsx
+++ fundamentals/src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//App.jsx siempre estará en un proyecto de React. Este es el componente principal
+//Los componentes son funciones de JS que retornan contenido HTML 
 import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+  return(
+    //comentario de js - parte de logica
+    //Los fragment son como cajas - pones el contenido del componente y no genera ningun elemento en el DOM
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    {/* comentario a nivel HTML */}
+    <h1>Hola Mundo</h1>
+    <p>Hola, esta es la clase introductoria de fundamentos</p>
     </>
   )
 }
-
 export default App
